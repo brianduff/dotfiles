301a68627d25cbfbb5a64f7aec37df786b1a3ea2
Brian Duff <cairndubh@gmail.com>
2019-04-22 16:34:52 -0700

    Add mod_android

---
+++ shell/shell/shared/mod_android.sh
@@ -0,0 +1,7 @@
+export ANDROID_SDK_ROOT=/usr/local/share/android-sdk
+export ANDROID_NDK_ROOT=/usr/local/share/android-sdk/ndk-bundle
+
+
+export ANDROID_HOME=$ANDROID_SDK_ROOT
+export ANDROID_NDK_REPOSITORY=$ANDROID_NDK_ROOT
+export ANDROID_SDK=$ANDROID_HOME
