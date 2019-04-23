304aa0ca7dc64f945fe7e3efb03ac4e67f82fe10
Brian Duff <cairndubh@gmail.com>
2019-04-22 17:03:06 -0700

    Remove google3 go commands, fix dotfiles go command

--- shell/shell/shared/mod_go.sh
+++ shell/shell/shared/mod_go.sh
@@ -5,56 +5,12 @@ g::home() {
   cd $HOME
 }
 
-riker::subcommand google3 "Go to the nearest google3 dir"
-g::google3() {
-  if [ -d "google3" ]; then
-    cd google3
-  else
-    g3
-  fi
-}
-
 riker::subcommand fig "Go to the fig directory"
 g::fig() {
   g4d dev_fig
 }
 
-riker::subcommand pointy "Go to the pointy source"
-g::pointy() {
-  go google3
-  cd experimental/users/bduff/java/com/google/pointy
-}
-
-riker::subcommand pointy "Go to the dotfiles dir"
+riker::subcommand dotfiles "Go to the dotfiles dir"
 g::dotfiles() {
   cd $HOME/dotfiles
-}
-
-#
-# With no argument: go to the nearest ancestor google3 directory.
-# With one argument: go to the google3 directory under the specified
-#   perforce client directory (assuming clients are in $HOME/src/clientname)
-#
-g3() {
-  oldpwd=$PWD
-
-  if [ "" != "$1" ]; then
-    if [ -d $HOME/src/$1/google3 ]; then
-      cd $HOME/src/$1/google3
-    else
-      if [ -d /usr/local/google/$USER/src/$1/google3 ]; then
-         cd /usr/local/google/$USER/src/$1/google3
-      else
-         echo "No google3 in $HOME/src/$1 or /usr/local/google/$USER/src/$1"
-      fi
-    fi
-  else
-    while [ "`basename $PWD`" != "google3" -a "`basename $PWD`" != "/" ]; do
-      cd ..
-    done
-    if [ "`basename $PWD`" = "/" ]; then
-      # if all else fails, go to the dev_fig directory.
-      g fig
-    fi
-  fi
 }
