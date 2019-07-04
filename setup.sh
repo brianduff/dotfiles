48893476616afb1f236891046c6a3f2f7fdb1634
Brian Duff <cairndubh@gmail.com>
2019-07-04 16:36:57 -0700

    Rewrite most of setup.sh to be a bit more clever (e.g. install stow by default)

--- setup.sh
+++ setup.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
 
-# Before installing, need to first install brew, then brew install stow, if linux.
-# TODO(bduff): do this here if stow not available?
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-stow bash farpoint git hg shell
-echo "Installed homedir bits"
+# MacOS specific bits
+if [ "$(uname)" == "Darwin" ]; then
+  if ! [ -x "$(command -v brew)" ]; then
+    echo "Installing brew..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+  if ! [ -x "$(command -v stow)" ]; then
+    echo "Installing stow"
+    brew install stow
+  fi
 
-if [ -d "$HOME/Library/Application Support/Code/User" ]; then
-  mv -f $HOME/Library/Application\ Support/Code/User/settings.json $HOME/Library/Application\ Support/Code/User/settings.json.bak
-  ln -s $HOME/dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-  echo "Installed vscode settings"
+  if [ -d "$HOME/Library/Application Support/Code/User" ]; then
+    echo "Installing VSCode Settings"
+    mv -f $HOME/Library/Application\ Support/Code/User/settings.json $HOME/Library/Application\ Support/Code/User/settings.json.bak
+    ln -s $DIR/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+  else
+    echo "Skipped installing VSCode Settings. Install VS Code and run setup.sh again"
+  fi
+
+  if [ ! -f "$HOME/Library/Application Support/iTerm2/DynamicProfiles/iterm-profiles.json" ]; then
+    echo "Installing iTerm 2 Dynamic Profiles"
+    mkdir -p "$HOME/Library/Application Support/iTerm2/DynamicProfiles"
+    ln -s $DIR/config/iterm-profiles.json "$HOME/Library/Application Support/iTerm2/DynamicProfiles/iterm-profiles.json"
+  fi
+
+else
+  # Assuming linux :{}
+  if ! [ -x "$(command -v stow)" ]; then
+    echo "Installing stow"
+    sudo apt-get install stow
+  fi
 fi
+
+
+echo "Linking home dir config files"
+pushd $DIR > /dev/null
+stow bash farpoint git hg shell
+popd > /dev/null
+
+
+echo "Setup is done!"
